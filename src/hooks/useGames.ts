aa7815fa77506cc658f45278c78c578d6b776af0
Junaid Burke <46665119+JunaidBurke@users.noreply.github.com>
2024-01-18 20:50:29 -0500

    Refactor API client and update hooks

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,9 @@
-import { FetchResponse } from "../services/api-client";
+import APIClient, { FetchResponse } from "../services/api-client";
 import { GameQuery } from "../App";
-import apiClient from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
 import { Platform } from "./usePlatforms";
 
+const apiClient = new APIClient("/games");
 export interface Game {
   id: number;
   name: string;
@@ -17,18 +17,14 @@ const useGames = (gameQuery: GameQuery) =>
   useQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
     queryFn: () =>
-      apiClient
-        .get<FetchResponse<Game>>("/games", {
-          params: {
-            genres: gameQuery.genre?.id,
-            parent_platforms: gameQuery.platform?.id,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText,
-          },
-        })
-        .then((res) => res.data),
-    // staleTime: 1000 * 60 * 60 * 24,
-    // initialData: { count: 0, results: [] },
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genre?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+        },
+      }),
   });
 
 export default useGames;
