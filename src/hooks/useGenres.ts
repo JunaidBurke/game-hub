d0aa09398681009345ac5a34e6dd93c2fbf7b73a
Junaid Burke <46665119+JunaidBurke@users.noreply.github.com>
2024-01-19 09:50:15 -0500

    Refactor GenreList and useGenres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 
-const apiClient = new APIClient("/genres");
+const apiClient = new APIClient<Genre>("/genres");
 
 export interface Genre {
   id: number;
